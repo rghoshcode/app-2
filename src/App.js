d3edcd40eae45650c8209a5a1c0cef7b6e84cb79
Ranajoy Ghosh <ranajoy.ghosh@gmail.com>
2021-06-12 23:15:22 +0200

    test commit 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save test to reload.
+          Edit <code>src/App.js</code> and save test test2 test to reload.
         </p>
         <a
           className="App-link"
